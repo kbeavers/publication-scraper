3052a4cb6cc6be24354835fd6160858a8c91231a
kbeavers <kbeavers@tacc.utexas.edu>
2026-02-25 12:12:30 -0600

    Add UT System keywords and crossref max pages to config.py

--- pubscraper/config.py
+++ pubscraper/config.py
@@ -5,8 +5,29 @@ LOGGER_LEVEL = "INFO"
 
 PUBMED_SEARCH_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
 PUBMED_SUMMARY_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
-CROSSREF_URL = "https://api.crossref.org/works"
 PUBMED_FETCH_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+CROSSREF_URL = "https://api.crossref.org/works"
+CROSSREF_MAX_PAGES = 5
 
-WS_NAME = "utrc_active_allocations"
 TIME_SLEEP = 0.4
+
+UT_SYSTEM_KEYWORDS = [
+    "university of texas at arlington",
+    "university of texas at austin",
+    "university of texas at dallas",
+    "university of texas at el paso",
+    "university of texas permian basin",
+    "university of texas rio grande valley",
+    "university of texas at san antonio",
+    "university of texas at tyler",
+    "ut southwestern medical center",
+    "university of texas medical branch at galveston",
+    "university of texas medical branch",
+    "uthealth houston",
+    "ut health houston",
+    "ut health san antonio",
+    "ut md anderson cancer center",
+    "md anderson cancer center",
+    "ut health science center at tyler",
+    "texas advanced computing center",
+]
